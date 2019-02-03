0db2b36d933611d00d72f803f71e3d79b1c29b54
UltraMask <38486835+UltraMask@users.noreply.github.com>
2019-02-03 20:09:37 +0900

    Update AppList.cpp
    :Release Notes:
    Pull the code from github.com/webosose for notificationmgr
    (https://github.com/webosose/notificationmgr/pull/2)
    
    :Detailed Notes:
    add a line to set s_applist_instance to null when AppList's destructor is called
    
    :Testing Performed:
    fixed and built on official build 1768
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-79028] Pull Requests on github.com/webosose for notificationmgr
    
    Change-Id: I900565ced346f991a065074134c7fa8d4ea0524f

--- src/AppList.cpp
+++ src/AppList.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2018 LG Electronics, Inc.
+// Copyright (c) 2013-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ AppList::AppList()
 
 AppList::~AppList()
 {
-
+	s_applist_instance = 0;
 }
 
 AppList* AppList::instance()
